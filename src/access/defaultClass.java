864ec7176d6a522bcc8f0f6611208ca4c9d7520e
lb <75319125+byeongsuLEE@users.noreply.github.com>
2025-09-15 23:59:40 +0900

    java - 접근 제어자

---
+++ src/access/defaultClass.java
@@ -0,0 +1,17 @@
+package access;
+
+/**
+ * 작성자  : lbs
+ * 날짜    : 2025-09-15
+ * 풀이방법
+ **/
+
+
+public class defaultClass {
+    public static void main(String[] args) {
+
+    }
+}
+class defaultClass2 {}
+
+
